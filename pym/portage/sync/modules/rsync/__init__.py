a814a177b7a019d05612f5a0136d13c2f897f75e
Brian Dolbec <dolsen@gentoo.org>
2014-02-07 23:14:52 -0800

    Extend the module_spec with the new functions.

--- pym/portage/sync/modules/rsync/__init__.py
+++ pym/portage/sync/modules/rsync/__init__.py
@@ -14,8 +14,20 @@ module_spec = {
 			'name': "rsync",
 			'class': "RsyncSync",
 			'description': __doc__,
-			'functions': ['sync',],
-			'func_desc': {'sync': 'Performs rsync transfers on the repository'}
+			'functions': ['sync', 'new', 'exists'],
+			'func_desc': {
+				'sync': 'Performs rsync transfers on the repository',
+				'new': 'Creates the new repository at the specified location',
+				'exists': 'Returns a boolean if the specified directory exists',
+				},
+			'func_parameters': {
+				'kwargs': {
+					'type': dict,
+					'description': 'Standard python **kwargs parameter format' +
+						'Please refer to the sync modules specs at ' +
+						'"https://wiki.gentoo.org:Project:Portage" for details',
+					},
+				},
 			}
 		}
 	}
