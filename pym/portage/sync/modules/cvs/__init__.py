376ac10ee9ce7f3460466a200335ab071e6057e4
Chris Reffett <creffett@gentoo.org>
2014-02-11 12:27:27 -0500

    Bring CVS module up to current module spec
    Add exists(), new(), _sync() as in the git and rsync classes. Clean up
    output style to use logger and writemsg_level instead of straight print
    statements

--- pym/portage/sync/modules/cvs/__init__.py
+++ pym/portage/sync/modules/cvs/__init__.py
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 """CVS plug-in module for portage.
-   Performs a git pull on the repo
+Performs a cvs up on repositories
 """
 
 
@@ -14,8 +14,21 @@ module_spec = {
 			'name': "cvs",
 			'class': "CVSSync",
 			'description': __doc__,
-			'functions': ['sync',],
-			'func_desc': {'sync': 'Performs a cvs up on the repo'}
-			}
+			'functions': ['sync', 'new', 'exists'],
+			'func_desc': {
+				'sync': 'Performs a cvs up on the repository',
+				'new': 'Creates the new repository at the specified location',
+				'exists': 'Returns a boolean of whether the specified dir ' +
+					'exists and is a valid CVS repository',
+			},
+			'func_parameters': {
+				'kwargs': {
+					'type': dict,
+					'description': 'Standard python **kwargs parameter format' +
+						'Please refer to the sync modules specs at ' +
+						'"https://wiki.gentoo.org:Project:Portage" for details',
+				},
+			},
 		}
 	}
+}
