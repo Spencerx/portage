Spencerx/portage

commit 378cc415b5f2c883b17f5cc2b08d49ce21b8557c

bintree: run trust helper when any binrepos.conf has verify-signature enabled too
If any binary hosts in binrepos.conf have `verify-signature` enabled,
we need to run the trust helper. Also, drop the comment because this
isn't a problem now with the simplified logic of binrepos.conf handling
for this (see c3fa8f90327227df7b1fd098cf1a977ccacf3c42).

Bug: https://bugs.gentoo.org/969187
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/bintree.py
@@ -1387,11 +1387,10 @@ class binarytree:
         self._remote_has_index = False
         self._remotepkgs = {}
 
-        if "binpkg-request-signature" in self.settings.features:
-            # This is somewhat broken, we *should* run the trust helper always
-            # when binpackages are involved, not only when we refuse unsigned
-            # ones. (If the keys have expired we end up refusing signed but
-            # technically invalid packages...)
+        need_trust_helper = "binpkg-request-signature" in self.settings.features or any(
+            repo.verify_signature for repo in self._binrepos_conf.values()
+        )
+        if need_trust_helper:
             if not pretend and self.dbapi.writable and portage.data.secpass >= 2:
                 self._run_trust_helper()
             gpkg_only = True

commit 966be93bad73b2d609381c542c06692d62e5d54b

bintree: add commentary for gpkg_only in _populate_local
It would be wrong to check binrepos.conf here, so the only config we can
go off is FEATURES="binpkg-request-signature", unlike _populate_remote.

Bug: https://bugs.gentoo.org/980835
Fixes: 378cc415b5f2c883b17f5cc2b08d49ce21b8557c
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/bintree.py
@@ -988,6 +988,8 @@ class binarytree:
         # missing digests.
         minimum_keys = self._pkgindex_keys.difference(self._pkgindex_hashes)
 
+        # We only need to check the FEATURES flag here, as they're all
+        # local binpkgs. binrepos.conf isn't relevant as it's for remote.
         if "binpkg-request-signature" in self.settings.features:
             gpkg_only = True
         else: